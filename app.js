e9e04003bf4d660a0f93671a34e40e48f08523d7
Segun Ajanaku <sajanaku@eminenttechology.com>
2022-05-05 09:13:07 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+
+const port=3000;
+
+app.use(express.static(`${__dirname}/public`));
+app.get("/", (req, res) => {
+    res.sendFile(`${__dirname}/index.html`, (err) => {
+        if(err) {
+            console.get(error);
+            res.end(err.message);
+        }
+    });
+});
+
+
+app.use(express.static(`${__dirname}/public`));
+app.get("/privacy", (req, res) => {
+    res.sendFile(`${__dirname}/privacy.html`, (err) => {
+        if(err) {
+            console.get(error);
+            res.end(err.message);
+        }
+    });
+});
+
+
+app.listen(process.env.PORT || port, () =>{
+    console.log('Server started');
+  
+})
